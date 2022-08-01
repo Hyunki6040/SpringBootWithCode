6e9ecc1596898bb36ca4de454e0691ef88d2afe9
user1 <hkgo6040@gmail.com>
2022-08-01 16:09:09 +0900

    commit

--- club/src/main/java/org/zerock/club/ClubApplication.java
+++ club/src/main/java/org/zerock/club/ClubApplication.java
@@ -2,8 +2,10 @@ package org.zerock.club;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing //BaseEntity를 상속받기 위해서 추
 public class ClubApplication {
 
     public static void main(String[] args) {
